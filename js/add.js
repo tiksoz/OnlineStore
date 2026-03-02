4ca483b52f12295a559742676aae50b2607a2587
Gizem Tiksoz <giz.tiksoz@gmail.com>
2026-03-02 23:50:07 +0300

    updated add.js

--- js/add.js
+++ js/add.js
@@ -1,4 +1,5 @@
 const buttons = document.querySelectorAll(".button2");
+
 buttons.forEach(button=>{
   button.addEventListener("click", function(e){
   e.preventDefault();
@@ -7,7 +8,7 @@ buttons.forEach(button=>{
   const name1 = card.querySelector("h5").textContent;
   const priceText = card.querySelector("span").textContent;
   const price = Number(priceText.replace(" TL", ""));
-  const img = card.querySelector("card-img-top").src;
+  const img = card.querySelector(".card-img-top").src;
   const category1 = card.dataset.category;
   const id = Number(card.dataset.id);
 
@@ -23,3 +24,7 @@ buttons.forEach(button=>{
   console.log(product)
   })
 })
+
+
+
+
